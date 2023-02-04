00636558b6806a8329fb9135f1c57255a333d520
J <118442144+model-map@users.noreply.github.com>
2023-02-04 13:23:17 +0530

    Color slider created grids Next: Allow drag left/right to color/delete grid

--- script.js
+++ script.js
@@ -13,45 +13,46 @@ slider.addEventListener('change',(e)=>{
 
 function destroyGrid(){
     const children=document.querySelectorAll('.gridChild');
-    if (children){
-        children.forEach((child)=>container.removeChild(child))
-    };
+    children.forEach((child)=>container.removeChild(child));
 }
 
 function createGrid(value){
+    container.style['grid-template-columns']=`repeat(${value},1fr)`;
     for (i=1;i<=value**2;i++){
         const div=document.createElement('div');
-        div.classList.add("gridChild");
+        div.classList.add('gridChild');
         container.appendChild(div);
-        container.style['grid-template-columns']=`repeat(${value},1fr)`;
     }
+    colorGrid();
 }
 
 // COLOURING GRID ELEMENTS
 const body=document.querySelector('body');
-const gridChildren=document.querySelectorAll('.gridChild');
 
-gridChildren.forEach((child)=>{
-    child.addEventListener('mouseover',()=>{
-        child.style['cursor']="pointer";
-    });
-
-    child.addEventListener('mouseup',(e)=>{
-        console.log("BUTTON ",e.button);
-        switch(e.button){
-            case 0:
-                child.classList.add("gridChild--colored");
-                child.style['background-color']=`hsl(${colorRandomise()})`;
-                break;
-
-            case 2:
-                child.classList.remove("gridChild--colored");
-                child.style['background-color']=`${body.style['background-color']}`;
-                break;
-        }
+function colorGrid(){
+    gridChildren=document.querySelectorAll('.gridChild');
+    gridChildren.forEach((child)=>{
+        child.addEventListener('mouseover',()=>{
+            child.style['cursor']="pointer";
+        });
+    
+        child.addEventListener('mouseup',(e)=>{
+            console.log("BUTTON ",e.button);
+            switch(e.button){
+                case 0:
+                    child.classList.add("gridChild--colored");
+                    child.style['background-color']=`hsl(${colorRandomise()})`;
+                    break;
+    
+                case 2:
+                    child.classList.remove("gridChild--colored");
+                    child.style['background-color']=`${body.style['background-color']}`;
+                    break;
+            }
+        })
+    
     })
-
-})
+}
 
 function colorRandomise(){
     const hue=Math.floor(Math.random()*360);
