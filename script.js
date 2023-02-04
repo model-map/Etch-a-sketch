a47ceaf8fa545619fdce1f1ba945d8fc2b946354
J <118442144+model-map@users.noreply.github.com>
2023-02-04 10:30:48 +0530

    Create grid using slider input

--- script.js
+++ script.js
@@ -6,5 +6,22 @@ const canvasSize=[4,8,16,32,64];
 slider.addEventListener('change',(e)=>{
     const value=canvasSize[parseInt(e.target.value)];
     slidervalue.textContent=`${value}x${value}`;
-    
+    console.log("value: ",value);
+    destroyGrid();
+    createGrid(value);
 });
+
+function destroyGrid(){
+    const children=document.querySelectorAll('.gridChild');
+    if (children){
+        children.forEach((child)=>container.removeChild(child))};
+}
+
+function createGrid(value){
+    for (i=1;i<=value**2;i++){
+        const div=document.createElement('div');
+        div.classList.add("gridChild");
+        container.appendChild(div);
+        container.style['grid-template-columns']=`repeat(${value},1fr)`;
+    }
+}
