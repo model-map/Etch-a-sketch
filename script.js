cf323e3c8e8a4251894618aa57c629f852bd2cf6
J <118442144+model-map@users.noreply.github.com>
2023-02-04 13:55:57 +0530

    Add easter egg

--- script.js
+++ script.js
@@ -61,3 +61,23 @@ function colorRandomise(){
     const color=`${hue},${saturation}%,${lightness}%`;
     return color;
 }
+
+// ADD EASTER EGG
+function bgColor(){
+    const elements=document.querySelectorAll('h3, h1, h4');
+    body.style['background-color']=`hsl(${colorRandomise()})`;
+    elements.forEach((element)=>{
+        element.style['color']=`hsl(${colorRandomise()})`;
+    })
+}
+
+const easteregg=document.querySelector('.easteregg');
+easteregg.addEventListener('mouseover',()=>{
+    const bgInterval=setInterval(bgColor,1000);
+    easteregg.addEventListener('mouseout',()=>{
+        clearInterval(bgInterval);
+/*         body.style['background-color']=`#4B231B`;
+        h1.style['color']="yellow";
+        p.style['color']=`#bfa602`; */
+    })
+});
